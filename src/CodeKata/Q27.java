45183d32e68897050ae4756b5e2985a06d281ae6
Kihong_Kim <rlghd0157gmail.compwd>
2024-12-31 15:06:09 +0900

    Q27

---
+++ src/CodeKata/Q27.java
@@ -0,0 +1,15 @@
+package CodeKata;
+
+public  class Q27 {
+    public String solution(String phone_number) {
+        String answer = "";
+        for (int i = 0; i < phone_number.length(); i++) {
+            if (i < phone_number.length() - 4) {
+                answer += "*";
+            } else {
+                answer += phone_number.charAt(i);
+            }
+        }
+        return answer;
+    }
+}
