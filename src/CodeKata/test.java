9a7c9fd08e1bfc61df729967750b49349c2abf5f
Kihong_Kim <rlghd0157gmail.compwd>
2025-01-09 20:15:27 +0900

    Q46

--- src/CodeKata/test.java
+++ src/CodeKata/test.java
@@ -1,22 +1,6 @@
 package CodeKata;
 
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 public class test {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        String t = sc.nextLine();
-        String p = sc.nextLine();
-        int answer = 0;
-        for (int i = 0; i < t.length(); i++) {
-            String str = t.substring(t.indexOf(i),i+p.length());
-            System.out.println(str);
-/*
-            if (Integer.parseInt(str) <= Integer.parseInt(p)) {
-                answer++;
-            }
-*/
-        }
-    }
-}
