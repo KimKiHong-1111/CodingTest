8707a5ca91ac603b4c08e2072314776e0af8110d
KimKiHong-1111 <rlghd0157@gmail.com>
2025-03-18 15:51:16 +0900

    Q15649 n과m í

---
+++ src/CodeKata/Q59.java
@@ -0,0 +1,8 @@
+package CodeKata;
+
+public class Q59 {
+    public int solution(int n, int m, int[] section) {
+        int answer = 0;
+        return answer;
+    }
+}
--- src/codingtest/Q1546.java
+++ src/codingtest/Q1546.java
@@ -3,6 +3,7 @@ package codingtest;
 import java.util.Scanner;
 
 public class Q1546 {
+
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
