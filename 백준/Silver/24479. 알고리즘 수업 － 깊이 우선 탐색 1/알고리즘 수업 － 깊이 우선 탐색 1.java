7bc5dc8f221bb31eb664eca30c2bf4dbd99ba946
KHKim <rlghd0157@gmail.com>
2025-03-21 11:31:06 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 936 ms, Memory: 89544 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.java
@@ -0,0 +1,59 @@
+import java.io.*;
+import java.util.*;
+ 
+public class Main {
+    
+    static ArrayList<ArrayList<Integer>> graph = new ArrayList<>();
+    static int count;
+    static int[] checked;
+    
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        
+        int n = Integer.parseInt(st.nextToken());
+        int m = Integer.parseInt(st.nextToken());
+        int r = Integer.parseInt(st.nextToken());
+        
+        checked = new int[n+1];
+        
+        for (int i = 0; i <= n; i++) {
+            graph.add(new ArrayList<>());
+        }
+        
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            
+            graph.get(u).add(v);
+            graph.get(v).add(u);
+        } 
+        
+        for(int i = 1; i <= n; i++) {
+            Collections.sort(graph.get(i));
+        }
+        
+        count = 1;
+        dfs(r);
+        for(int i = 1; i <= n; i++) {
+            sb.append(checked[i]).append("\n");
+        }
+        bw.write(sb.toString());
+        bw.flush();
+    }
+    
+    private static void dfs(int node) {
+        checked[node] = count;
+        for(int i = 0; i < graph.get(node).size(); i++) {
+            int newNode = graph.get(node).get(i);
+            if(checked[newNode] == 0) {
+                count++;
+                dfs(newNode);
+            }
+        }
+    }
+}
