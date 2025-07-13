4d7c349d8f913ed60a71af58bf7c907adb4d9d60
KHKim <rlghd0157@gmail.com>
2025-07-13 09:30:59 +0900

    [level 0] Title: 순서 바꾸기, Time: 2.75 ms, Memory: 88.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181891. 순서 바꾸기/순서 바꾸기.java
@@ -0,0 +1,16 @@
+import java.util.ArrayList;
+
+class Solution {
+    public int[] solution(int[] num_list, int n) {
+		int[] answer = {};
+		ArrayList<Integer> list = new ArrayList<>();
+		for(int i = n ; i < num_list.length; i++) {
+			list.add(num_list[i]);
+		}
+		for (int i = 0; i < n ; i++) {
+			list.add(num_list[i]);
+		}
+		answer = list.stream().mapToInt(i -> i).toArray();
+		return answer;
+	}
+}
