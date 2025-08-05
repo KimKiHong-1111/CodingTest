0c2706d45606ac075a24756a2b75f7dd8fadb9d3
KHKim <rlghd0157@gmail.com>
2025-08-05 22:41:55 +0900

    [level 0] Title: 문자열 잘라서 정렬하기, Time: 22.30 ms, Memory: 82.2 MB -BaekjoonHub

---
+++ 프로그래머스/0/181866. 문자열 잘라서 정렬하기/문자열 잘라서 정렬하기.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+class Solution {
+    public String[] solution(String myString) {
+		String[] answer = myString.split("x");
+		
+		Arrays.sort(answer);
+		
+
+
+		return answer;
+	}
+}
